77819f1caa1bd0383a8ab6962bd2ac48a04632ea
Carmen Ramajo <carmen.ramajo@gruposantander.com>
2024-09-23 17:14:16 +0200

    greeting

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,16 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public static void greetUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name + "!");
     }
 }
