627f8e37c76752d90f6cc1a9216f924ad60ba7cc
Carmen Ramajo <carmen.ramajo@gruposantander.com>
2024-09-23 17:21:28 +0200

    date

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,13 @@
 package com.example;
 
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public static void getDate() {
+        System.out.println(java.time.LocalDate.now()); 
     }
 }
